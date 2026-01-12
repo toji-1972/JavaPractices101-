43eb5662b47fac66543f77c2de63d522d8b1760f
Seyyid <seyyidefekaraman47@gmail.com>
2026-01-12 13:43:13 +0300

    git

---
+++ src/L02_DataUsageAndVariables/C04_VariableNamingConvension.java
@@ -0,0 +1,25 @@
+package L02_DataUsageAndVariables;
+
+public class C04_VariableNamingConvension {
+
+	public static void main(String[] args) {
+
+		int age = 40;
+		int Age = 68;
+		int aGe = 79;
+		int aGE = 19;
+
+		// Variable names starts with lowercase letters
+		// But if you start with uppercase letters
+		// Java code doesn't recognize it as an error
+
+
+		System.out.println(aGE); // 40
+		System.out.println(age); // 19
+		// System.out.println(AgE); // We don't have a variable like that
+
+
+
+
+	}
+}
