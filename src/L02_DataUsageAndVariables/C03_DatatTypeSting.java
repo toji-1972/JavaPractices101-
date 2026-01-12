641db6de754dd0d061581ce38cb0c2b58a89c7d3
Seyyid <seyyidefekaraman47@gmail.com>
2026-01-12 13:34:14 +0300

    git

---
+++ src/L02_DataUsageAndVariables/C03_DatatTypeSting.java
@@ -0,0 +1,25 @@
+package L02_DataUsageAndVariables;
+
+public class C03_DatatTypeSting {
+
+	public static void main(String[] args) {
+
+		String str = "Java İs Beautiful";
+
+		int number = 20;
+
+
+		System.out.println(str); // java is Beautiful
+
+
+		// Non-primitive data types has built-in methods
+
+		System.out.println(str.toLowerCase()); // java is beautiful
+		System.out.println(str.toUpperCase()); // JAVA İS BEAUTİFUL
+
+
+
+
+
+	}
+}
