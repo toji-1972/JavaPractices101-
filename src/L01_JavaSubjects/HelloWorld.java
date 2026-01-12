51612874850ccfa90bdcf411e27a3429f3d18506
Seyyid <seyyidefekaraman@gmail.com>
2026-01-12 11:41:15 +0300

    git

---
+++ src/L01_JavaSubjects/HelloWorld.java
@@ -0,0 +1,20 @@
+package L01_JavaSubjects;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+
+		// It's been a while since I made comment
+
+		System.out.println("Hello World!!!");
+
+		// Double slash allows you to write comments. Comments cannot be executed by java.
+
+		/*Every line we wrote between astesriks
+		 are recognised by java as comments, and is not executed
+		 by Java
+		*/
+	}
+}
+
