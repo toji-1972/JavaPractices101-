a42f0641e4d1d307e6a57ea10e6c337221d6f9fb
Seyyid <seyyidefekaraman47@gmail.com>
2026-01-13 08:36:05 +0300

    git

---
+++ src/L03_gettingDataFromUSer/Cl2_GettingInfoFromUser.java
@@ -0,0 +1,51 @@
+package L03_gettingDataFromUSer;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Cl2_GettingInfoFromUser {
+
+	public static void main(String[] args) {
+
+		// Question 2 - Get the user's name, surname, and age and print it in the following format.
+
+		//      Name : John
+		//      Surname : Doe
+		//		Age : 44
+		//      Your login has successfully made
+
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Please enter your name");
+
+		String name = scanner.nextLine();
+
+		System.out.println("Please enter your surname");
+
+		String surname = scanner.nextLine();
+
+		System.out.println("Please enter your age");
+
+		int age = scanner.nextInt();
+
+		System.out.println(
+				"Name : " + name +
+				"\nSurname : " + surname +
+				"\nAge : " + age +
+				"\nYour login has successfully made"
+
+		);
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
